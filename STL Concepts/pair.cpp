fda7336f82468be214f72f503e00c4d1308d1f40
Nusrat Jahan Raisa <jahanthinks@gmail.com>
2025-09-13 11:34:38 +0600

    Update pair.cpp

--- STL Concepts/pair.cpp
+++ STL Concepts/pair.cpp
@@ -1,1 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define yes cout<<"YES\n";
+#define no cout<<"NO\n";
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 
+
+
+int main(){ 
+    optimize();  
+    
+    pair <string,vector<int>> p;
+    p.first = "raisa";
+    p.second = {1,2,3};
+
+    for(int i=0;i<p.second.size();i++){
+        cout<<p.second[i]<<endl; 
+    }
+    for(auto u: p.second){
+        cout<<u<<" ";
+    }
+
+    return 0;
+}
